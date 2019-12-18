2f14b71a80a96ad08a2b26d7c72390ff64977fa8
Daniyal Khan Haider <daniyal.khan@iesjoandaustria.org>
2019-12-18 19:00:23 +0100

    third part

---
+++ wordpress_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo
+echo
+echo
+echo Script by Daniyal Khan
+echo 
+echo
+echo
+
+#Download the latest WordPress installation package
+wget https://wordpress.org/latest.tar.gz
+
+#Unzip and unarchive the installation package
+tar -xzf latest.tar.gz
+
+#Iniciamos el mysql
+mysql -u root -p
+
+#Right now follow the README to create a db user
+
+echo
+echo
+echo
+echo FOLLOW THE README TO CREATE A USER IN THE DB 
+echo
+echo
+echo
+
+#
+
+#
+
+#
